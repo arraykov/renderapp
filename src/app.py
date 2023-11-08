3bf181fe5c7c686eb201f7087270bc80e4fceac5
arraykov <arraykov@gmail.com>
2023-11-08 14:20:13 +0200

    commit

--- src/app.py
+++ src/app.py
@@ -117,7 +117,6 @@ sidebarDef = {
             "iconKey": "columns",
             "toolPanel": "agColumnsToolPanel",
             "toolPanelParams": {
-                    "suppressColumnFilter": True,
                     "suppressColumnSelectAll": True,
                     "suppressColumnExpandAll": False,
                     "suppressPivotMode": True,
@@ -165,86 +164,10 @@ external_stylesheets = [
 ]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
 
 app.layout = html.Div([
-    dcc.Store(id='last-clicked-button', storage_type='session'),
-
-    html.Div(id='navbar', style={'display': 'flex', 'justify-content': 'center', 'gap': '10px'}, children=[
-        html.Button('CEF Dashboard', id='btn-cefs', n_clicks=0),
-        html.Button('Research Center', id='btn-researchcenter', n_clicks=0),
-        html.Button('Page 1', id='btn-page1', n_clicks=0),
-        html.Button('Page 2', id='btn-page2', n_clicks=0),
-        html.Button('Page 3', id='btn-page3', n_clicks=0),
-    ]),
-    
-    html.Div(id='content', style={'padding-top': '50px'}),
+    generate_dashboard()
 ])
-
-@app.callback(
-    [Output('last-clicked-button', 'data'),
-     Output('content', 'children')],
-    [Input('btn-researchcenter', 'n_clicks'),
-     Input('btn-cefs', 'n_clicks'),    
-     Input('btn-page1', 'n_clicks'),
-     Input('btn-page2', 'n_clicks'),
-     Input('btn-page3', 'n_clicks')],
-    [State('last-clicked-button', 'data')]
-)
-
-def update_content(n_dashboard, n_pdf, n_page1, n_page2, n_page3, last_clicked):
-    ctx = dash.callback_context
-    button_id = ctx.triggered_id or last_clicked
-
-    if button_id == 'btn-cefs':
-        # Replace generate_dashboard() with the actual function that generates the dashboard
-        return 'btn-cefs', generate_dashboard()
-    elif button_id == 'btn-researchcenter':
-    # Combine PDFs and podcasts in one layout
-        return 'btn-researchcenter', html.Div([
-    # Podcasts side by side
-    html.Div([
-        html.Iframe(
-            src="https://open.spotify.com/embed/show/1T6xOGR2S5tY6bZ7XbpAC3?utm_source=generator&theme=0",
-            width="35%",
-            height="100px",
-        ),
-        html.Iframe(
-            src="https://open.spotify.com/embed/show/48EaweC3xjFdgfg2XAMMLX?utm_source=generator",
-            width="35%",
-            height="100px",
-        ),
-        html.Iframe(
-            src="https://open.spotify.com/embed/show/0f4MGmaXmzedzoqm4P5aXd?utm_source=generator",
-            width="35%",
-            height="100px",
-        ),
-    ], style={'display': 'flex', 'justify-content': 'space-between'}),
-
-    # PDFs side by side below the podcasts
-    html.Div([
-        html.Iframe(
-            src="https://share.refinitiv.com/assets/newsletters/Morning_News_Call/MNC_US.pdf",
-            width="50%",
-            height="750px",
-        ),
-        html.Iframe(
-            src="https://share.refinitiv.com/assets/newsletters/The_Day_Ahead/TDA_NAM.pdf",
-            width="50%",
-            height="750px",
-        ),
-    ], style={'display': 'flex', 'justify-content': 'space-between', 'padding-top': '2px'}),
-])
-
-    elif button_id == 'btn-page1':
-        # Add content for Page 1
-        return button_id, html.Div([])  # Update with Page 1 content
-    elif button_id in ['btn-page2', 'btn-page3']:
-        # Add content for Page 2 and Page 3
-        return button_id, html.Div([])  # Update with Page 2 and Page 3 content
-    
-    # Default return statement if no buttons have been clicked
-    return last_clicked, html.Div([])  # Update with default content
 
 if __name__ == '__main__':
     app.run_server(debug=False, host='0.0.0.0')
